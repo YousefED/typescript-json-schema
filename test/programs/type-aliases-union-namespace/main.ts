3a6a9dec6778395209e7319071d5e0bf247e738d
Dominik Moritz <domoritz@gmail.com>
2016-11-02 18:46:16 -0700

    Fix linting errors

--- test/programs/type-aliases-union-namespace/main.ts
+++ test/programs/type-aliases-union-namespace/main.ts
@@ -1,9 +1,9 @@
 
 export namespace Cardinal {
-    export const NORTH: 'north' = 'north';
-    export const SOUTH: 'south' = 'south';
-    export const EAST: 'east' = 'east';
-    export const WEST: 'west' = 'west';
+    export const NORTH: "north" = "north";
+    export const SOUTH: "south" = "south";
+    export const EAST: "east" = "east";
+    export const WEST: "west" = "west";
 }
 
 export type Cardinal = typeof Cardinal.NORTH | typeof Cardinal.SOUTH | typeof Cardinal.EAST | typeof Cardinal.WEST;

3b34447813990352e54ae949df1e6116b0b0bc5f
Dominik Moritz <domoritz@gmail.com>
2016-11-02 17:19:09 -0700

    Don’t export type from namespace

--- test/programs/type-aliases-union-namespace/main.ts
+++ test/programs/type-aliases-union-namespace/main.ts
@@ -1,16 +1,12 @@
 
 export namespace Cardinal {
     export const NORTH: 'north' = 'north';
-    export type NORTH = typeof NORTH;
     export const SOUTH: 'south' = 'south';
-    export type SOUTH = typeof SOUTH;
     export const EAST: 'east' = 'east';
-    export type EAST = typeof EAST;
     export const WEST: 'west' = 'west';
-    export type WEST = typeof WEST;
 }
 
-export type Cardinal = Cardinal.NORTH | Cardinal.SOUTH | Cardinal.EAST | Cardinal.WEST;
+export type Cardinal = typeof Cardinal.NORTH | typeof Cardinal.SOUTH | typeof Cardinal.EAST | typeof Cardinal.WEST;
 
 export interface MyModel {
     direction: Cardinal;

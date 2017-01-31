d1ab21886313be2bfaebc10afcb3f379bf6f1416
Otger <otgerrogla@gmail.com>
2017-01-31 18:59:28 +0100

    Fixed #89 (@type was not working)

--- test/programs/type-primitives/main.ts
+++ test/programs/type-primitives/main.ts
@@ -11,7 +11,9 @@ class MyObject {
 
     /** @TJS-type integer */
     integer1: number      = 1;
-    integer2: integer     = 1;
+    /** @type integer */
+    integer2: number      = 1;
+    integer3: integer     = 1;
 
     string1: string       = "defaultValue";
 

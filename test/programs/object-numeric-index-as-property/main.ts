401dcf885461b0dae9e6621fa86c7271ff4a0ed6
Andrew Boyton <aboyton@users.noreply.github.com>
2019-05-15 10:39:13 +1000

    Correct schema for mapped object with number index (#277)
    * Properly transform object with numeric index (#275)
    * Added tests and fixed failing tests (#275)

--- test/programs/object-numeric-index-as-property/main.ts
+++ test/programs/object-numeric-index-as-property/main.ts
@@ -3,7 +3,14 @@ interface Target {
     [index: number]: number;
   };
   objInterface: IndexInterface;
+  indexInType: { [index in number]?: number };
+  indexInInline: { [index in number]: number };
+  indexInPartialType: IndexInPartial;
+  indexInPartialInline: { [index in number]?: number };
 }
 interface IndexInterface {
   [index: number]: number;
 }
+
+type IndexIn = { [index in number]: number };
+type IndexInPartial = { [index in number]?: number };

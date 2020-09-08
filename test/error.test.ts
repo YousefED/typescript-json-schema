33c00c3f979164cf1dec446272e0a0e607bc5be1
Rohan Talip <RohanTalip@users.noreply.github.com>
2020-09-07 22:40:30 -0700

    Ran "yarn style"

--- test/error.test.ts
+++ test/error.test.ts
@@ -3,9 +3,13 @@ import { exec, getDefaultArgs } from "../typescript-json-schema";
 
 describe("error", () => {
     it("error-check", () => {
-        assert.throws(() => {
-            // This needs a valid path. "dates" chosen basically at random
-            exec("test/programs/dates/", "MyObject", getDefaultArgs());
-        }, Error, "No output definition. Probably caused by errors prior to this?");
+        assert.throws(
+            () => {
+                // This needs a valid path. "dates" chosen basically at random
+                exec("test/programs/dates/", "MyObject", getDefaultArgs());
+            },
+            Error,
+            "No output definition. Probably caused by errors prior to this?"
+        );
     });
 });

ee05287321e6dd7a924ca5162a86f312646476c1
Martin Tichovsky <martin.tichovsky@sinnerschrader.com>
2021-01-19 15:19:28 +0100

    fix: update require regex

--- typescript-json-schema.ts
+++ typescript-json-schema.ts
@@ -13,7 +13,7 @@ const REGEX_FILE_NAME_OR_SPACE = /(\bimport\(".*?"\)|".*?")\.| /g;
 const REGEX_TSCONFIG_NAME = /^.*\.json$/;
 const REGEX_TJS_JSDOC = /^-([\w]+)\s+(\S|\S[\s\S]*\S)\s*$/g;
 const REGEX_GROUP_JSDOC = /^[.]?([\w]+)\s+(\S|\S[\s\S]*\S)\s*$/g;
-const REGEX_REQUIRE = /^(\s+)?require\((\'[a-zA-Z0-9.\/_-]+\'|\"[a-zA-Z0-9.\/_-]+\")\)(\.([a-zA-Z0-9_-]+))?(\s+)?/;
+const REGEX_REQUIRE = /^(\s+)?require\((\'@?[a-zA-Z0-9.\/_-]+\'|\"@?[a-zA-Z0-9.\/_-]+\")\)(\.([a-zA-Z0-9_$]+))?(\s+|$)/;
 const NUMERIC_INDEX_PATTERN = "^[0-9]+$";
 
 export function getDefaultArgs(): Args {

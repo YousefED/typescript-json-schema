812d2a4dab64416fd074382827990a57541cd2c7
Arnaud Courtecuisse <arnaud.courtecuisse@webedia-group.com>
2021-03-10 02:22:35 +0100

    Use promise to resolve execution

--- test/error.test.ts
+++ test/error.test.ts
@@ -2,14 +2,13 @@ import { assert } from "chai";
 import { exec, getDefaultArgs } from "../typescript-json-schema";
 
 describe("error", () => {
-    it("error-check", () => {
-        assert.throws(
-            () => {
-                // This needs a valid path. "dates" chosen basically at random
-                exec("test/programs/dates/", "MyObject", getDefaultArgs());
-            },
-            Error,
-            "No output definition. Probably caused by errors prior to this?"
-        );
+    it("error-check", async () => {
+        try  {
+            await exec("test/programs/dates/", "MyObject", getDefaultArgs());
+            assert.fail("Expected exec to fail");
+        } catch (err) {
+            assert.instanceOf(err, Error);
+            assert.equal(err.message, "No output definition. Probably caused by errors prior to this?");
+        }
     });
 });
